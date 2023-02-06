1b6308c45926a513ab0c0955db8762b1ef622681
Fred Mark Baldeviso <fredmark.baldeviso@gmail.com>
2023-02-07 04:18:47 +0800

    Adds responsive design
    Changes dimension on CSS
    Scripts addition of pixel and fixes bugs to make responsive design

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
-let dimensions = 20;
+let dimensions = 16;
 let sketchPad = document.querySelector("#sketch-pad");
+
+
+// window.addEventListener("resize", resizePad);
+
+// function resizePad() {
+//     // let padArea = document.querySelector("#pad-area");
+//     // let padAreaWidth = padArea.offsetWidth;
+//     // padArea.style.height = `${padAreaWidth}px`;
+//     // console.log(padArea.style.height);
+//     let padRowWidth = document.querySelector(".pad-row").offsetWidth;
+//     let pixelDim = document.querySelectorAll(".pixel-dim");
+//     pixelDim.forEach(pixel => {
+//         pixel.style.width = `${padRowWidth / dimensions}px`;
+//         pixel.style.height = `${padRowWidth / dimensions}px`;
+//     })
+// }
+
+
 
 
 
@@ -9,11 +27,17 @@ for (let i = 0; i < dimensions; i++) {
     let padRow = document.createElement("div");
     padRow.classList.add("pad-row");       
     sketchPad.appendChild(padRow);
-
+    
     for (let i = 0; i < dimensions; i++) {
         let pixel = document.createElement("div");
         pixel.classList.add("pixel-dim");
         padRow.appendChild(pixel);
     }
+    // let pixelDim = document.querySelectorAll(".pixel-dim");
+    // pixelDim.forEach(pixel => {
+    //     pixel.style.width = `${padRowWidth / dimensions}px`;
+    //     pixel.style.height = `${padRowWidth / dimensions}px`;
+
+    // });
 
 }
